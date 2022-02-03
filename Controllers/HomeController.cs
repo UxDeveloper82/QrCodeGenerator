1fcc8ba96d6e85eb15c925a76efa925676d852d8
UxDeveloper82 <electronicsmf@aol.com>
2022-02-02 21:18:43 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using BarcodeLib;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QrCodeGenerator.Models;
+using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QrCodeGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult GenerateBarcode(string code="256987")
+        {
+
+            Barcode barcode = new Barcode();
+            Image img = barcode.Encode(TYPE.CODE39, code, Color.Black, Color.White, 250, 100);
+            var data = ConvertImageToBytes(img);
+            return File(data, "image/jpeg");
+
+        }
+
+        private byte[] ConvertImageToBytes(Image img)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                img.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                return ms.ToArray();
+            }  
+        }
+        public IActionResult GenerateQrCode(string code="Welcome to DotNet") 
+        {
+            QRCodeGenerator qRCodeGenerator = new QRCodeGenerator();
+            QRCodeData qRCodeData = qRCodeGenerator.CreateQrCode(code, QRCodeGenerator.ECCLevel.Q);
+            QRCode qRCode = new QRCode(qRCodeData);
+            Bitmap bitmap = qRCode.GetGraphic(15);
+            var bitmapBytes = ConvertBitmapToBytes(bitmap);
+            return File(bitmapBytes, "image/jpeg");
+        }
+
+        private byte[] ConvertBitmapToBytes(Bitmap bitmap)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                return ms.ToArray();
+            }
+        }
+    }
+}
